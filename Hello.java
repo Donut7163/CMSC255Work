1d187f21132553fb441c4dd2d3a13569daf7ec81
Donut7163 <54003117+Donut7163@users.noreply.github.com>
2022-08-24 11:24:33 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,21 @@
+/*********************************************************************************
+* Printinator 9k
+**********************************************************************************
+* Vaughn Baumgartner
+* 8/24/2022
+* CMSC 255 Lab 01
+**********************************************************************************/
+
+public class Hello {
+	public static void main(String[] args) {
+	
+		System.out.println("Vaughn");
+		System.out.println("Printinator 9k");
+		System.out.println("Lab 01");
+		System.out.println("8/24/2022");
+		System.out.println("CMSC 255 Section 01");
+		System.out.println();
+		
+	}
+	
+}
